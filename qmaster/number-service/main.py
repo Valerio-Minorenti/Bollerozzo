39242a069fcad4c3591acee3f5f4d49740bb3a5a
dariopattu <dario.pattumelli@alcampus.it>
2025-07-07 11:33:03 +0200

    Update main.py

--- qmaster/number-service/main.py
+++ qmaster/number-service/main.py
@@ -1,18 +1,23 @@
 from fastapi import FastAPI
+import redis
+import os
 
 app = FastAPI()
 
-# Dizionario per tenere traccia dei numeri per ciascuna coda
-counters = {}
+# Config Redis
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 @app.get("/")
 def root():
     return {"message": "Number Service is running"}
 
 @app.get("/next-number/{queue_id}")
-def next_number(queue_id: str):
-    if queue_id not in counters:
-        counters[queue_id] = 1
-    else:
-        counters[queue_id] += 1
-    return {"next": counters[queue_id]}
+def get_next_number(queue_id: str):
+    # Chiave: es. queue:clinica:counter
+    key = f"queue:{queue_id}:counter"
+    next_number = r.incr(key)
+    return {"next": next_number}
+
