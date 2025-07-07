78c945cb536033e37c14c28068835b3e235c7b2d
Valerio-Minorenti <v.minorenti.02@gmail.com>
2025-07-07 11:31:41 +0200

    matteon

--- qmaster/number-service/main.py
+++ qmaster/number-service/main.py
@@ -2,6 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Dizionario per tenere traccia dei numeri per ciascuna coda
+counters = {}
+
 @app.get("/")
 def root():
     return {"message": "Number Service is running"}
+
+@app.get("/next-number/{queue_id}")
+def next_number(queue_id: str):
+    if queue_id not in counters:
+        counters[queue_id] = 1
+    else:
+        counters[queue_id] += 1
+    return {"next": counters[queue_id]}
